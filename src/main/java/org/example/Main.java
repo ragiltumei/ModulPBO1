8e6093b42d3d64b1e279a9854cf919ae13107976
ragiltumei <ragiltumeiw@gmail.com>
2024-03-24 02:09:25 +0700

    modul 1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,41 @@
+package org.example;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan nama: ");
+        String nama = input.nextLine();
+
+        System.out.print("Masukkan jenis kelamin (L/P) : ");
+        String jenisKelaminInput = input.nextLine();
+        String jenisKelamin;
+        if (jenisKelaminInput.equalsIgnoreCase("P")) {
+            jenisKelamin = "Perempuan";
+        } else if (jenisKelaminInput.equalsIgnoreCase("L")) {
+            jenisKelamin = "Laki-laki";
+        } else {
+            jenisKelamin = "Jenis kelamin tidak valid";
+        }
+
+        System.out.print("Masukkan tanggal lahir (YYYY-MM-DD): ");
+        String tanggalLahirInput = input.nextLine();
+        LocalDate tanggalLahir = LocalDate.parse(tanggalLahirInput);
+
+        LocalDate sekarang = LocalDate.now();
+        Period selisih = Period.between(tanggalLahir, sekarang);
+        int tahun = selisih.getYears();
+        int bulan = selisih.getMonths();
+
+        System.out.println("\nData Diri:");
+        System.out.println("Nama: " + nama);
+        System.out.println("Jenis Kelamin: " + jenisKelamin);
+        System.out.println("Tanggal Lahir: " + tanggalLahir);
+        System.out.println("Umur: " + tahun + " tahun " + bulan + " bulan");
+
+        input.close();
+    }
+}
